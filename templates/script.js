49fc375c568a169c0393e2410c83eb4a9f5d9a9b
Alessandra Avelino <alessavelino123@gmail.com>
2024-03-22 10:29:15 -0300

    att page

--- templates/script.js
+++ templates/script.js
@@ -1,9 +1,6 @@
 setTimeout(function(){
-    console.log("ok")
     let alert = document.getElementById("alert")
     if (alert) {
         alert.style.display = "none"
     }
 }, 3000);
-
-console.log("oi")
